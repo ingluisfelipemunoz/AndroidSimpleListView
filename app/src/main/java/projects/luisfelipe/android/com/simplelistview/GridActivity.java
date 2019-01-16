55e863bc8c9dda1af6e5f2f122f03ea043a53a77
JarvisXxX <eldrizzy@hotmail.com>
2019-01-15 22:15:04 -0600

    action_bar_menu
    Adding button to add an item to the grudview.

--- app/src/main/java/projects/luisfelipe/android/com/simplelistview/GridActivity.java
+++ app/src/main/java/projects/luisfelipe/android/com/simplelistview/GridActivity.java
@@ -2,6 +2,9 @@ package projects.luisfelipe.android.com.simplelistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -14,6 +17,8 @@ public class GridActivity extends AppCompatActivity {
 
     List<String> names;
     GridView gridView;
+    MyAdapter myAdapter;
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,31 +33,7 @@ public class GridActivity extends AppCompatActivity {
         names.add("Pedro");
         names.add("Jose");
         names.add("Fernando");
-        names.add("Felipe");
-        names.add("Luis");
-        names.add("Pedro");
-        names.add("Jose");
-        names.add("Fernando");
-        names.add("Felipe");
-        names.add("Luis");
-        names.add("Pedro");
-        names.add("Jose");
-        names.add("Fernando");
-        names.add("Felipe");
-        names.add("Luis");
-        names.add("Pedro");
-        names.add("Jose");
-        names.add("Fernando");
-        names.add("Felipe");
-        names.add("Luis");
-        names.add("Pedro");
-        names.add("Jose");
-        names.add("Fernando");
-        names.add("Felipe");
-        names.add("Luis");
-        names.add("Pedro");
-        names.add("Jose");
-        names.add("Fernando");
+
 
 
         //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
@@ -67,7 +48,31 @@ public class GridActivity extends AppCompatActivity {
         });
 
         //Enlace con el adaptador personalizado.
-        MyAdapter myAdapter = new MyAdapter(this, R.layout.grid_item, names);
+        myAdapter = new MyAdapter(this, R.layout.grid_item, names);
         gridView.setAdapter(myAdapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.add_item:
+                this.names.add("Added nro: "+(++count));
+                this.myAdapter.notifyDataSetChanged();
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+
 }
